2627cf688fcc306d805ab51f77da7635c369c4d8
Flaner6 <christosmpalotis@gmail.com>
2022-01-12 23:29:45 +0200

    first commit

---
+++ public/deck.js
@@ -0,0 +1,122 @@
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+
+
+
+
+class Card {
+    constructor(value, suit){
+        this.value = value;
+        this.suit = suit;
+ 
+
+        }
+    turn(){
+        return `${this['value']} of ${this['suit']} `
+    }
+}
+
+class MajorArcana {
+    constructor(major){
+        this.major = major;
+
+        }
+    turn(){
+        return this['major']
+    }
+    }
+
+class Deck{
+    constructor(){
+        let suits = ["Cups", "Pentacles", "Clubs", "Swords"];
+        let values = ['Ace','2','3','4','5','6','7','8','9','10','Paige','Knight','Queen','King'];
+        let majors = ['The Fool','The Magician','The High Priestess','The Empress','The Emperor','The Hierophant','The Lovers','The Chariot','Strength','The Hermit',
+        'Wheel of Fortune','Justice','The Hanged Man','Death','Temperance','The Devil','The Tower','The Star','The Moon','The Sun','Judgement','The World'];
+        this.cards = [];
+        for (let i = 0; i < suits.length; i++) {
+            for (let j = 0; j < values.length; j++){
+                this.cards.push(new Card(values[j],suits[i]).turn())
+            }
+        }
+        for (let i = 0; i < majors.length; i++){
+            this.cards.push(new MajorArcana(majors[i]).turn())
+        }
+        // console.log(this.cards)
+ 
+    }
+    count(){
+    return this.cards.length;
+    }
+    
+
+    remove_card(){
+  
+        let card = this.cards.pop();
+        return card;        
+    }
+    deal_card(card){
+        let rand = Math.random()*100;
+        if (rand > 40){
+            return card
+        }
+        else{
+            return `${card} reversed`
+        }
+    }
+
+    return_card(card){
+        this.cards.unshift(card)
+    }
+
+        
+        
+    
+
+    
+    
+    cross(){
+
+        shuffle(this.cards)
+        let cross = [];
+        for (let i = 1; i < 11; i++){
+            let removedCard = this.remove_card();
+            var crd = this.deal_card(removedCard);
+            this.return_card(removedCard)
+            console.log("position",i,":",crd);
+            cross.push(`Position ${i}: ${crd}\n`);            
+        }  
+        return cross;
+        
+
+
+        
+        
+    }
+
+    
+}
+
+let d = new Deck();
+
+
+
+
+function reading(){
+    document.getElementById("display-array").textContent = d.cross().join("\r\n");
+    }
