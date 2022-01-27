e44282a57d72d0240beb20da473189815ce2498c
Your Name <you@example.com>
2022-01-27 14:30:44 +0200

    new images

--- public/deck.js
+++ public/deck.js
@@ -45,7 +45,7 @@ class MajorArcana {
 class Deck{
     constructor(){
         let suits = ["Cups", "Pentacles", "Clubs", "Swords"];
-        let values = ['Ace','2','3','4','5','6','7','8','9','10','Paige','Knight','Queen','King'];
+        let values = ['Ace','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Page','Knight','Queen','King'];
         let majors = ['The Fool','The Magician','The High Priestess','The Empress','The Emperor','The Hierophant','The Lovers','The Chariot','Strength','The Hermit',
         'Wheel of Fortune','Justice','The Hanged Man','Death','Temperance','The Devil','The Tower','The Star','The Moon','The Sun','Judgement','The World'];
         this.cards = [];
